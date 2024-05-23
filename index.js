a0c5bcba500eb7e46c776efa7eb39b0d6d7b7835
Fergus <fergus.ampuan@gmail.com>
2024-05-23 10:46:57 +0800

    removed cron

--- index.js
+++ index.js
@@ -12,42 +12,6 @@ app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
-const schedule = require('node-schedule');
-
-// Schedule a job to run at 6:30 PM every day
-// schedule.scheduleJob('15 16 * * *', async function() {
-//   try {
-//     const response = await axios.get(`https://api.thingspeak.com/channels/${process.env.CHANNEL_ID}/feeds.json`, {
-//       params: {
-//         api_key: process.env.READ_API_KEY,
-//         results: 1 // Fetch only the most recent data point
-//       }
-//     });
-
-//     const data = response.data.feeds[0];
-//     const emailContent = `Latest data from ThingSpeak: ${JSON.stringify(data)}`;
-
-//     const transporter = nodemailer.createTransport({
-//       service: 'gmail', // e.g., 'gmail' or your email service
-//       auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS
-//       }
-//     });
-
-//     const mailOptions = {
-//       from: process.env.EMAIL_USER,
-//       to: process.env.RECIPIENT_EMAIL,
-//       subject: 'Latest ThingSpeak Data',
-//       text: emailContent
-//     };
-
-//     await transporter.sendMail(mailOptions);
-//     console.log('Email sent successfully');
-//   } catch (error) {
-//     console.error('Error fetching data or sending email:', error);
-//   }
-// });
 
 app.get('/', function (req, res) {
   res.render('index');  
